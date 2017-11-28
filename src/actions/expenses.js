03384991914064a70d05f0610ebc65060a7c7cf2
Apoorv Vardhan <vardhanapoorv@gmail.com>
2017-11-28 08:51:28 +0530

    Add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -48,6 +48,14 @@ id,
 updates
 });
 
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates));
+    });
+  };
+};
+
 // SET_EXPENSES
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
